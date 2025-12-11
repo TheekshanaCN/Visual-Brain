8314c12c377943fef58af1bf1a8f8f2cad19ea29
TheekshanaCN <theekshanachamodhya@gmail.com>
2025-12-11 22:43:44 +0530

    Defult Theme

--- components/newboard/app-sidebar.tsx
+++ components/newboard/app-sidebar.tsx
@@ -101,6 +101,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </SidebarMenuButton>
               )}
             </SidebarMenuItem>
+
           </SidebarMenu>
         </SidebarGroup>
 
